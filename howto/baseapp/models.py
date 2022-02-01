337be6b38762a08311a319d3575034ac7dc9ac4f
Nguyễn Anh Đô <nguyenanhdo200213@gmail.com>
2022-02-01 18:09:28 +0700

    Edit files

--- howto/baseapp/models.py
+++ howto/baseapp/models.py
@@ -1,3 +1,24 @@
+from configparser import MAX_INTERPOLATION_DEPTH
+from email.policy import default
+from sre_constants import CATEGORY_NOT_DIGIT
+from unicodedata import category
 from django.db import models
+from django.contrib.auth import get_user_model
+from taggit.models import Tag
 
+from ckeditor_uploader.fields import RichTextUploadingFormField
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Post(models.Model):
+    title = models.TextField(max_length=255)
+    content = RichTextUploadingFormField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    liked = models.ManyToManyField(get_user_model())
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    tags = Tag()
--- howto/howto/settings.py
+++ howto/howto/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts',
+    'baseapp',
+
+    'ckeditor',
+    'ckeditor_uploader',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +61,9 @@ ROOT_URLCONF = 'howto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,8 +127,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+CKEDITOR_UPLOAD_PATH = 'uploads'
